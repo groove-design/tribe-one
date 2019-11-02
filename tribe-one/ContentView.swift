99f6a748ef9ece76448eb8e96d695cfbcc680521
Oleg Kazantsev <ok020373@gmail.com>
2019-11-03 01:50:11 +1300

    Tabbed App template has been applied

---
+++ tribe-one/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  tribe-one
+//
+//  Created by Oleg Kazantsev on 3/11/19.
+//  Copyright © 2019 Groove Design. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
